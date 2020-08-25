005ac1ebf13ee9f2df806801c6e39978a71b11c8
maliha-mamtaz <maliha.mamtaz@northsouth.com>
2020-08-25 10:42:43 +0600

    2.asm 5.asm commit

---
+++ Lab_Task/Lab_5/2.asm
@@ -0,0 +1,64 @@
+.MODEL SMALL
+.STACK 100H
+
+
+.DATA
+  PROMPT DB 'Enter the Digit: $'
+  MSG DB 'The EnteRED Digit is: $'
+
+  
+.CODE
+  
+  MAIN PROC
+   
+    MOV AX, @DATA
+    MOV DS, AX
+    
+    LEA DX, PROMPT
+    MOV AH, 9
+    INT 21H
+    
+    MOV AH, 1
+    INT 21H
+    
+    MOV BL,AL
+   
+    MOV AH,2
+    MOV DL,0DH
+    INT 21H
+           
+    MOV DL,0AH
+    INT 21H        
+   
+   LEA DX,MSG
+   MOV AH,9
+   INT 21H
+   
+   CMP BL,30H
+   
+   JZ @ZERO
+   JG @POSITIVE
+   JL @NEGATIVE
+   
+   @ZERO:
+   MOV DL,'Z'
+   JMP @PRINT
+   
+   @POSITIVE:
+   MOV DL,'P'
+   JMP @PRINT
+   
+   @NEGATIVE:
+   MOV DL,'N'
+   JMP @PRINT
+   
+   @PRINT:
+   MOV AH,2 
+   INT 21H
+   
+   MOV AH,4CH
+   INT 21H
+   
+   MAIN ENDP
+  END MAIN
+   
